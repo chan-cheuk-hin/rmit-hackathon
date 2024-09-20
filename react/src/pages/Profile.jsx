e55e9441573d7545de876e1b3c3de3566293b29d
NickDoublet <nicholas.doublet@gmail.com>
2024-09-21 09:33:59 +1000

    add basic profile page layout

--- react/src/pages/Profile.jsx
+++ react/src/pages/Profile.jsx
@@ -1,21 +1,55 @@
 import '../css/Profile.css';
-import { Container, Card, Button } from 'react-bootstrap';
+import { Container, Card, Button, Badge, Row, Col } from 'react-bootstrap';
 
 const Profile = () => {
 	return (
 		<Container>
-			<h1>Profile</h1>
-			<Card style={{ width: '18rem' }}>
-				<Card.Img variant='top' src='https://via.placeholder.com/150' />
+			<Card className='profile-card'>
 				<Card.Body>
 					<Card.Title>John Doe</Card.Title>
-					<Card.Text>
-						Web Developer with a passion for creating responsive and modern web
-						applications.
-					</Card.Text>
-					<Button variant='primary'>Follow</Button>
+					<div>
+						<Card.Text>Follows:</Card.Text>
+						<Badge pill variant='primary'>
+							Science
+						</Badge>
+						<Badge pill variant='secondary'>
+							Technology
+						</Badge>
+						<Badge pill variant='success'>
+							Art
+						</Badge>
+					</div>
 				</Card.Body>
 			</Card>
+			<Container>
+				<Row
+					className='d-flex justify-content-center align-items-center'
+					style={{ height: '200px' }}
+				>
+					<Col md={6} className='text-center'>
+						<div className='p-4 border'>Project 1</div>
+						<Button variant='primary'>Link to Project</Button>
+					</Col>
+					<Col md={6} className='text-center'>
+						<div className='p-4 border'>Project 2</div>
+						<Button variant='primary'>Link to Project</Button>
+					</Col>
+				</Row>
+
+				<Row
+					className='d-flex justify-content-center align-items-center'
+					style={{ height: '200px' }}
+				>
+					<Col md={6} className='text-center'>
+						<div className='p-4 border'>Project 3</div>
+						<Button variant='primary'>Link to Project</Button>
+					</Col>
+					<Col md={6} className='text-center'>
+						<div className='p-4 border'>Project 4</div>
+						<Button variant='primary'>Link to Project</Button>
+					</Col>
+				</Row>
+			</Container>
 		</Container>
 	);
 };
