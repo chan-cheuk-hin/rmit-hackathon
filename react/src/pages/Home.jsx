0706e7305500779244978b0f3d83ccb165241815
DarioKillenberger <s3787455@student.rmit.edu.au>
2024-09-21 00:43:33 +1000

    added sample placeholders for event and notifications until we get the actual code merged

--- react/src/pages/Home.jsx
+++ react/src/pages/Home.jsx
@@ -4,17 +4,22 @@ import './../css/Home.css'
 import Calendar from '../components/Calendar'
 
 const Home = () => {
-    const [collapsed, setCollapsed] = useState(false);
+    const [collapsed, setCollapsed] = useState(true);
+    const eventArray = [<div>sampleExpo1</div>, <div>sampleExpo2</div>, <div>sampleExpo3</div>];
+    const notifArray = [<div>sampleExpo1</div>, <div>sampleExpo2</div>, <div>sampleExpo3</div>];
 
     return (
         <div className='homeGrid'>
             <button onClick={() => setCollapsed(!collapsed)} className='collapseBtn'>{collapsed ? "open notifications" : "close notifications"}</button>
-            <div className={`sideBar ${collapsed ? "collapsed" : "uncollapsed"}`}>
             
+            <div className={`sideBar ${collapsed ? "collapsed" : "uncollapsed"}`}>
+                {notifArray.map((item, i) => <div className={`placeholderNotif ${collapsed ? "hidden" : "visible"}`} key={i}>{item}</div>)}
             </div>
+
             <div className='mainContent'>
                 <h1 className='title'>Upcoming Events: </h1>
                 <Calendar className="calendarContainer"/>
+                {eventArray.map((item, i) => <div className='placeholderExpo' key={i}>{item}</div>)}
             </div>
         </div>
     )
