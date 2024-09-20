2543d980db73bb5a3eb41ad12215686a09d91498
DarioKillenberger <dario13ad@posteo.de>
2024-09-20 20:20:13 +1000

    tweaked some stuff, started on homepage

--- react/src/App.js
+++ react/src/App.js
@@ -1,16 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/navBar';
 import Home from './pages/Home';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
       <Routes>
          <Route path='/' element={<Home/>} />
          <Route path='/home' element={<Home/>} />
+         <Route path='/profile' element={<Profile/>} />
        </Routes>
      
     </div>
--- react/src/index.js
+++ react/src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/navBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    
     <BrowserRouter>
+    <Navbar/>
     <App />
     </BrowserRouter>
   </React.StrictMode>
