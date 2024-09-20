e86de87313f67c1c64aa0ec73ee03a6598d94a35
DarioKillenberger <dario13ad@posteo.de>
2024-09-20 22:04:03 +1000

    made home notification sidepanel collapse

--- react/src/pages/Home.jsx
+++ react/src/pages/Home.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import './../Home.css'
 
 const Home = () => {
+    const [collapsed, setCollapsed] = useState(false);
+
     return (
         <div className='homeGrid'>
-            <h1>Home</h1>
+            <button onClick={() => setCollapsed(!collapsed)} className='collapseBtn'>test</button>
+            <div className={`sideBar ${collapsed ? "collapsed" : "uncollapsed"}`}>
+            
+            </div>
+            <div className='mainContent'></div>
         </div>
     )
 }
 
-export default Home
+export default Home 
