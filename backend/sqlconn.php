f22cb6eddc438b3a87fc9ff4c3bfcccd5426961b
Andrew <chan.ch.andrew@gmail.com>
2024-09-21 19:30:21 +1000

    ad project to db

--- backend/sqlconn.php
+++ backend/sqlconn.php
@@ -46,6 +46,73 @@ class SqlConnection {
     }
 
 
+    public static function AddProject($id, $title, $description, $status, $est){
+
+        try{
+
+            $conn = new PDO("mysql:host=localhost;dbname=expo", "andrew", "andrew1105");
+
+            $stmt = $conn->prepare("
+            INSERT INTO `Projects` 
+            (`id`, `title`, `description`, `completion_status`, `est_completion_date`) VALUES
+            (:id, :title, :description, :status, :est)
+            ");
+            $stmt->bindParam(':id', $id, PDO::PARAM_STR);
+            $stmt->bindParam(':title', $title, PDO::PARAM_STR);
+            $stmt->bindParam(':description', $description, PDO::PARAM_STR);
+            $stmt->bindParam(':status', $status, PDO::PARAM_STR);
+            $stmt->bindParam(':est', $est, PDO::PARAM_STR);
+            
+
+            $stmt->execute();
+
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+
+
+    }
+
+    public static function GetUpcomingExpos(){
+
+        try{
+
+            $conn = new PDO("mysql:host=localhost;dbname=expo", "andrew", "andrew1105");
+            $stmt = $conn->query("SELECT * from Expos;");
+
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            $upcomming = [];
+
+            foreach($rows as $row){
+
+                $dtstring = $row['datetime'];
+                $date = explode(" ", $dtstring)[0];
+                $data = explode("-", $date);
+
+                if(
+                    (date('Y') == $data[0]) && 
+                    (date("m") == $data[1])
+                )                {
+                    array_push($upcomming, $row);
+                }
+
+            }
+
+            return json_encode(["data" => $upcomming]);
+
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }        
+
+    }
+
+    public static function GetCompletion(){
+
+
+    }  
+
+
 
     public static function ValidateLogin($email, $password){
 
